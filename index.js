2533a67e49ea2281fa2dd9d3279f31e898945896
Areefshaik2002 <Areefshaik2002@gmail.com>
2024-07-31 12:23:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+// var a = 50;
+// b;
+// console.log(a);
+
+// var b=100;
+// console.log(b);
+
+// // var is a global scope
+// var c = 34;
+// {
+//     console.log(c);
+// }
+// console.log(c);
+
+// // d=20;
+// // console.log(d);
+// let d =23;
+// console.log(d);
+
+// // let is a block scope
+// let e=54;
+// {
+//     console.log(e);
+// }
+// console.log(e);
+
+// {
+//     let f=102;
+//     console.log(f);
+// }
+// // console.log(f);
+// // {
+
+
+// // const is a block scope
+// const g =100;
+// g=200;
+// console.log(g);
+// }
+// console.log(g);
+
+changeTextHello = 67;
+console.log(changeTextHello);
+
+document.write("STARTED LEARNING JS");
+alert("STARTED LEARNING JS");
+prompt("Options are:");
+confirm("Are you sure you want to");
